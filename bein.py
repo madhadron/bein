da0dd3ff11e408a7b6d43e4adbbb36da25073bc9
Frederick Ross <github.20.madhadron@spamgourmet.com>
2010-11-15 17:04:27 +0100

    Added immutability to database.

--- bein.py
+++ bein.py
@@ -449,6 +449,94 @@ class MiniLIMS:
         FOR EACH ROW WHEN (OLD.repository_name != NEW.repository_name) BEGIN
              SELECT RAISE(FAIL, 'Cannot change the repository name of a file.');
         END""")
+        self.db.execute("""
+        CREATE VIEW file_immutability AS 
+        SELECT file as id, count(execution) > 0 as immutable from execution_use group by file
+        """)
+        self.db.execute("""
+        CREATE VIEW execution_outputs AS
+        select execution.id as execution, file.id as file 
+        from execution left join file 
+        on execution.id = file.origin_value
+        and file.origin='execution'
+        """)
+        self.db.execute("""
+        CREATE VIEW execution_immutability AS
+        SELECT eo.execution as id, ifnull(max(fi.immutable),0) from
+        execution_outputs as eo left join file_immutability as fi
+        on eo.file = fi.immutable
+        group by id
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_file_delete BEFORE DELETE ON file 
+        FOR EACH ROW WHEN 
+            (SELECT immutable FROM file_immutability WHERE id = OLD.id) = 1
+        BEGIN
+            SELECT RAISE(FAIL, 'File is immutable; cannot delete it.'); 
+        END
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_argument_delete BEFORE DELETE ON argument
+        FOR EACH ROW WHEN 
+            (SELECT immutable FROM execution_immutability WHERE id = OLD.execution) = 1 
+        BEGIN 
+            SELECT RAISE(FAIL, 'Execution is immutable; cannot delete argument.'); 
+        END	   
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_argument_update BEFORE UPDATE ON argument
+        FOR EACH ROW WHEN
+            (SELECT immutable FROM execution_immutability WHERE id = OLD.execution) = 1 
+        BEGIN
+            SELECT RAISE(FAIL, 'Execution is immutable; cannot update command arguments.'); 
+        END
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_command_delete BEFORE DELETE ON program
+        FOR EACH ROW WHEN 
+            (SELECT immutable FROM execution_immutability WHERE id = OLD.execution) = 1 
+        BEGIN
+            SELECT RAISE(FAIL, 'Execution is immutable; cannot delete command.'); 
+        END
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_command_update BEFORE UPDATE ON program
+        FOR EACH ROW WHEN 
+            (SELECT immutable FROM execution_immutability WHERE id = OLD.execution) = 1
+        BEGIN
+            SELECT RAISE(FAIL, 'Execution is immutable; cannot update commands.'); 
+        END
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_execution_delete BEFORE DELETE ON execution 
+        FOR EACH ROW WHEN
+            (SELECT immutable FROM execution_immutability WHERE id = OLD.id) = 1
+        BEGIN
+            SELECT RAISE(FAIL, 'Execution is immutable; cannot delete.'); 
+        END
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_execution_update BEFORE UPDATE ON execution
+        FOR EACH ROW WHEN
+            (SELECT immutable FROM execution_immutability WHERE id = OLD.id) = 1 AND 
+            (OLD.id != NEW.id OR OLD.started_at != NEW.started_at OR OLD.finished_at != NEW.finished_at
+             OR OLD.temp_dir != NEW.temp_dir) 
+        BEGIN
+            SELECT RAISE(FAIL, 'Execution is immutable; cannot update anything but description.'); 
+        END
+        """)
+        self.db.execute("""
+        CREATE TRIGGER prevent_immutable_file_update BEFORE UPDATE on file 
+        FOR EACH ROW WHEN 
+            (SELECT immutable FROM file_immutability WHERE id = old.id) = 1 AND 
+            (OLD.id != NEW.id OR OLD.external_name != NEW.external_name OR 
+             OLD.repository_name != NEW.repository_name OR 
+             OLD.created != NEW.created OR OLD.origin != NEW.origin OR 
+             OLD.origin_value != NEW.origin_value) 
+        BEGIN 
+            SELECT RAISE(FAIL, 'File is immutable; cannot update except description.'); 
+        END
+        """)
         self.db.commit()
 
     def copy_file_to_repository(self,src):
