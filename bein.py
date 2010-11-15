dc32a1d8c3f329e96174f722071f69d31731229b
Frederick Ross <github.20.madhadron@spamgourmet.com>
2010-11-15 13:25:00 +0100

    Wrote search_executions and copy_file for MiniLIMS

--- bein.py
+++ bein.py
@@ -1,15 +1,19 @@
-"""bein
+"""bein - LIMS and workflow manager for bioinformatics
 by Frederick Ross <madhadron@gmail.com>
 
-bein.py contains a miniature LIMS (Laboratory Information Management System) and a workflow manager.  It was written for the Bioinformatics and Biostatistics Core Facility of the Ecole Polytechnique Federale de Lausanne.
+Bein contains a miniature LIMS (Laboratory Information Management System) and a workflow manager.  It was written for the Bioinformatics and Biostatistics Core Facility of the Ecole Polytechnique Federale de Lausanne.  It is aimed at processes just complicated enough where the Unix shell becomes problematic, but not so large as to justify all the machinery of big workflow managers like KNIME or Galaxy.
 
 There are three core classes you need to understand:
 
 program
-* Used as a decorator, is callable, and has methods for nonblocking and lsf
+-------
+Programs are functions which are turned into callable objects by the @program decorator.  They provide a very clean way of binding programs into bein so that all the machinery of workflow management and LIMS functions properly.
 
-Execution
-* Actually created using the execution() function.  Stores all information about an ongoing execution.
+execution
+---------
+The actual class is Execution, but it is generally created with the execution contextmanager.  An execution tracks all the information about a run of a given set of programs.  It corresponds roughly to a script in shell.
+
+Executions are run in a temporary directory.  The execution provides methods to pull in files from the LIMS for use, write files back to the LIMS, and tracks all arguments, pids, outputs, and exit codes of programs run during the execution.
 
 MiniLIMS
 * Database and file directory object.  Never edit a MiniLIMS repository by hand.
@@ -197,7 +201,8 @@ class Execution(object):
     def finish(self):
         self.finished_at = int(time.time())
     def use(self, fileid):
-        return [x for (x,) in self.lims.db.execute("select exportfile(?,?)", (fileid, self.exwd))][0]
+        return [x for (x,) in self.lims.db.execute("select exportfile(?,?)", 
+                                                   (fileid, self.exwd))][0]
 
 
 @contextmanager
@@ -215,7 +220,19 @@ def execution(lims = None):
 # MiniLIMS
 
 
-class MiniLims:
+class MiniLIMS:
+    """Encapsulates a database and directory to track executions and files.
+
+    Tries to check if it doesn't exist, and creates it if not.  Format of the LIMS repository: name (an SQLite3 database) and name.files (a directory containing the files it refers to).  You should never edit the files directory by hand or you may leave the database inconsistent.
+
+    Give structure of the tables: execution, program, argument, and file.  Make a couple remarks about the triggers.
+
+    Adds Python functions to it (you can't really use the LIMS sensibly without them, so it's limited to Python.  You can easily add your own versions.  The functions are importfile, deletefile, and exportfile.  Give their behaviors.
+
+    'write' writes an execution to the LIMS, including adding files from its working directory.
+
+    search_files looks up files in the LIMS
+    """
     def __init__(self, path):
         self.db = sqlite3.connect(path)
         self.file_path = path +".files"
@@ -227,6 +244,9 @@ class MiniLims:
         self.db.create_function("exportfile",2,self.export_file_from_repository)
 
     def initialize_database(self, db):
+        """Sets up a new MiniLIMS database.
+
+        """
         self.db.execute("""
         CREATE TABLE execution ( 
              id integer primary key, 
@@ -264,24 +284,37 @@ class MiniLims:
                origin text not null default 'execution', 
                origin_value integer default null
         )""")
-
-        self.db.commit()
         self.db.execute("""
         CREATE TRIGGER prevent_repository_name_change BEFORE UPDATE ON file
         FOR EACH ROW WHEN (OLD.repository_name != NEW.repository_name) BEGIN
              SELECT RAISE(FAIL, 'Cannot change the repository name of a file.');
         END""")
+        self.db.commit()
 
     def copy_file_to_repository(self,src):
+        """Copy a file src into the MiniLIMS repository.
+        
+        src can be a fairly arbitrary path, either from the CWD, or
+        using .. and other such shortcuts.  This function should only
+        be called from SQLite3, not Python.
+        """
         filename = unique_filename_in(self.file_path)
         shutil.copyfile(src,os.path.abspath(os.path.join(self.file_path,filename)))
         return filename
 
     def delete_repository_file(self,filename):
+        """Delete a file from the MiniLIMS repository.
+
+        This function should only be called from SQLite3, not from Python.
+        """
         os.remove(os.path.join(self.file_path,filename))
         return None
 
     def export_file_from_repository(self,fileid,dst):
+        """Write a file with id fileid to the directory dst.
+
+        This function should only be called from SQLite3, not Python.
+        """
         filename = unique_filename_in(dst)
         try:
             [repository_filename] = [x for (x,) in self.db.execute("select repository_name from file where id=?", 
@@ -293,7 +326,10 @@ class MiniLims:
             return None
 
     def write(self, ex, description = ""):
-        """Write an execution to the miniLims"""
+        """Write an execution to the MiniLIMS.
+
+        Give some details about where things are written.
+        """
         self.db.execute("""
                         insert into execution(started_at,finished_at,
                                               working_directory,description) 
@@ -318,7 +354,7 @@ class MiniLims:
     def search_files(self, with_text=None, older_than=None, newer_than=None, source=None):
         """Find files matching given criteria in the LIMS.
 
-        source should be a 2-tuple of ('execution',id), etc.
+        source should be a 2-tuple of ('execution',id), etc.  Explain the other fields.
         """
         source = source != None and source or (None,None)
         with_text = with_text != None and '%' + with_text + '%' or None
@@ -333,9 +369,42 @@ class MiniLims:
                                                source[0], source[1], source[0], source[1]))
         return [x for (x,) in matching_files]
 
+    def search_executions(self, with_text=None, started_before=None, started_after=None, ended_before=None, ended_after=None):
+        with_text = with_text != None and '%'+with_text+'%' or None
+        sql = """select id from execution where 
+                 (started_at <= ? or ? is null) and (started_at >= ? or ? is null) and
+                 (finished_at <= ? or ? is null) and (finished_at >= ? or ? is null) and
+                 (description like ? or ? is null)
+              """
+        matching_executions = [x for (x,) in self.db.execute(sql, (started_before, started_before,
+                                                                   started_after, started_after,
+                                                                   ended_before, ended_before,
+                                                                   ended_after, ended_after,
+                                                                   with_text, with_text))]
+        if with_text != None:
+            sql = """select distinct execution from argument where argument like ?"""
+            matching_programs = [x for (x,) in self.db.execute(sql, (with_text,))]
+        else:
+            matching_programs = []
+        return list(set(matching_executions+matching_programs))
+
+    def copy_file(self, fileid):
+        try:
+            [(external_name, repository_name, description)] = [x for x in self.db.execute("select external_name,repository_name,description from file where id = ?", (fileid, ))]
+            new_repository_name = unique_filename_in(self.file_path)
+            [x for x in self.db.execute("insert into file(external_name,repository_name,origin,origin_value) values (?,?,?,?)",
+                                        (external_name, new_repository_name, 'copy', fileid))]
+            [new_id] = [x for (x,) in self.db.execute("select last_insert_rowid()")]
+            shutil.copyfile(os.path.join(self.file_path, repository_name),
+                            os.path.join(self.file_path, new_repository_name))
+            self.db.commit()
+            return new_id
+        except ValueError, v:
+            raise ValueError("No such file id " + str(fileid))
+
 
 def get_ex():
-    m = MiniLims("test")
+    m = MiniLIMS("test")
     with execution(m) as ex:
 # #    f = bowtie(ex, '../test_data/selected_transcripts', '../test_data/reads-1-1')
         f = touch(ex)
