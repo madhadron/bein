bdaea1b7b583c6d083ed99640872e5ba62264d7b
Frederick Ross <github.20.madhadron@spamgourmet.com>
2010-12-03 16:44:23 +0100

    Updated versin number in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='bein',
-      version='0.1',
+      version='0.9',
       description='Miniature LIMS and workflow manager for bioinformatics',
       author='Fred Ross',
       author_email='madhadron@gmail.com',
