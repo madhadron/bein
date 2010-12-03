9aaf5c7399b86fbe0a472dc666f5310b1610642f
Frederick Ross <github.20.madhadron@spamgourmet.com>
2010-12-03 10:17:18 +0100

    Added a few changes to util.py, deleted unnecessary files.

--- bein/util.py
+++ bein/util.py
@@ -81,3 +81,51 @@ def add_figure(ex, figure_type='eps', description=""):
     filename = unique_filename_in() + '.' + figure_type
     f.savefig(filename)
     ex.add(filename, description)
+
+@program
+def sort_bam(bamfile):
+    filename = unique_filename_in()
+    return {'arguments': ['samtools','sort',bamfile,filename],
+            'return_value': filename + '.bam'}
+
+@program
+def index_bam(bamfile):
+    return {'arguments': ['samtools','index',bamfile],
+            'return_value': bamfile + '.bai'}
+
+def add_and_index_bam(ex, bamfile, description=""):
+    sort = sort_bam(ex, bamfile)
+    index = index_bam(ex, sort)
+    fileid = ex.add(sort, description=description)
+    ex.add(index, description=description + " (BAM index)",
+           associate_to_filename=sort, template='%s.bai')
+    return fileid
+
+@program
+def merge_bamfiles(files):
+    filename = unique_filename_in()
+    args = ['samtools','merge',filename]
+    args.extend(files)
+    return {'arguments': args,
+            'return_value': filename}
+
+@program
+def bowtie_build(files, index = None):
+    if index == None:
+        index = unique_filename_in()
+    if isinstance(files,list):
+        files = ",".join(files)
+    return {'arguments': ['bowtie-build', '-f', files, index],
+            'return_value': index}
+
+def add_bowtie_index(ex, files, description="", alias=None, index=None):
+    index = bowtie_build(ex, files, index=index)
+    touch(ex, index)
+    ex.add(index, description=description, alias=alias)
+    ex.add(index + ".1.ebwt", associate_to_filename=index, template='%s.1.ebwt')
+    ex.add(index + ".2.ebwt", associate_to_filename=index, template='%s.2.ebwt')
+    ex.add(index + ".3.ebwt", associate_to_filename=index, template='%s.3.ebwt')
+    ex.add(index + ".4.ebwt", associate_to_filename=index, template='%s.4.ebwt')
+    ex.add(index + ".rev.1.ebwt", associate_to_filename=index, template='%s.rev.1.ebwt')
+    ex.add(index + ".rev.2.ebwt", associate_to_filename=index, template='%s.rev.2.ebwt')
+    return index
