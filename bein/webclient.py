90435f062bfacc1685ff58b7da476e0bd1b56570
Frederick Ross <github.20.madhadron@spamgourmet.com>
2010-12-03 13:04:28 +0100

    Added README and license notices.

--- bein/webclient.py
+++ bein/webclient.py
@@ -1,3 +1,21 @@
+# bein/webclient.py
+# Copyright 2010 Frederick Ross
+
+# This file is part of bein.
+
+# Bein is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# Bein is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with bein.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 beinclient 0.1
 Fred Ross, <fred dot ross at epfl dot ch>
