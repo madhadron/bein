719213018122a8c967eef870d4fdd9898e91f1b2
Frederick Ross <ross@ptbbpc1.epfl.ch>
2011-02-10 15:38:38 +0100

    Updated test suites

--- test/test_util.py
+++ test/test_util.py
@@ -15,18 +15,18 @@ class TestBowtie(TestCase):
     def test_parallel_bowtie_local(self):
         with execution(None) as ex:
             bam = parallel_bowtie(ex, '../test_data/selected_transcripts', 
-                                  '../test_data/reads.raw', n_lines=500,
+                                  '../test_data/reads.raw', n_lines=250,
                                   via='local')
             sam = bam_to_sam(ex, bam)
             new_sam = remove_lines_matching(ex, '@PG', sam)
             new_bam = sam_to_bam(ex, new_sam)
-            self.assertEqual(md5sum(ex, new_bam), '7b7c270a3980492e82591a785d87538f')
+            self.assertEqual(md5sum(ex, new_bam), '2e6bd8ce814949075715b8ffddd1dcd5')
 
     def test_parallel_bowtie_lsf(self):
         if hostname_contains('vital-it.ch'):
             with execution(None) as ex:
                 bam = parallel_bowtie(ex, '../test_data/selected_transcripts', 
-                                      '../test_data/reads.raw', n_lines=500,
+                                      '../test_data/reads.raw', n_lines=250,
                                       via='lsf')
                 sam = bam_to_sam(ex, bam)
                 new_sam = remove_lines_matching(ex, '@PG', sam)
